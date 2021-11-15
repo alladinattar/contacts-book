fe54d447584939f0aa6b4fe6545fb14e50f6d7b4
Rinat <gitg>
2021-11-15 18:10:58 +0300

    start create templates

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from tg import tg
+from flask import Flask, render_template
 
-if __name__ == "__main__":
-    tg.start_bot()
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+app.run(debug=True)
