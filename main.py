f561921d5475bf1c5538a7721c9ea699e3ea1bf1
Rinat <gitg>
2021-11-16 23:26:14 +0300

    work on templates

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+app.secret_key = b'helloworld'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 @app.route("/")
 def home():
@@ -9,5 +19,11 @@ def home():
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/auth/login", methods=["POST"])
+def auth_login():
+    if request.method == "POST":
+        print(request.form["username"])
+    return "Hello world"
 
 app.run(debug=True)
